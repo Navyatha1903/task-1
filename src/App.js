f43752bb8cd071b52505450f455ed980254a63bb
Navyatha1903 <navyathapalagiri19@gmail.com>
2022-03-16 16:46:28 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Menu from './component/Menu';
 import List from './component/list';
-import Des from './component/wishlist';
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import uniReducer from './reducer/reducer'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import uniReducer from './reducer/reducer';
 import WishList from './component/wishlist';
 
-const URL ="http://universities.hipolabs.com/search?country=United+States"
+const URL = 'https://universities.hipolabs.com/search?country=United+States';
 
-
-function App(){
-  const[data,setData] = useState([]);
+function App() {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-      fetch(`${URL}`)
-      .then(res => res.json())
-      .then(out => {
-          console.log('out =', out)
-          setData(out)
+    fetch(`${URL}`)
+      .then((res) => res.json())
+      .then((out) => {
+        console.log('out =', out);
+        setData(out);
       })
-      .catch(err => console.log(err.message));
-  },[])
+      .catch((err) => console.log(err.message));
+  }, []);
 
-  return(
-    <Provider store={createStore(uniReducer,data)} >
-        <BrowserRouter>
-        <Menu/>
+  return (
+    <Provider store={createStore(uniReducer, data)}>
+      <BrowserRouter>
+        <Menu />
         <Routes>
-          <Route path={`/`} element={<List/>}/>
-          <Route path={`/list`} element={<List/>}/>
-          <Route path={`/wishlist`} element={<WishList/>}/>
+          <Route path={`/`} element={<List />} />
+          <Route path={`/list`} element={<List />} />
+          <Route path={`/wishlist`} element={<WishList />} />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
     </Provider>
-  )
+  );
 }
 
 export default App;
